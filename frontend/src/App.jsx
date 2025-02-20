a88795cb66768deb814e3370af00ed21a738e4f7
Mahfuzur Rahman <mxhfxzur.r@gmail.com>
2025-02-20 19:33:19 +0000

    simple routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
+import Favourites from "./pages/Favourites";
 import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favourites" element={<Favourites />} />
+      </Routes>
+    </main>
   );
 }
 
---
+++ frontend/src/pages/Favourites.jsx
@@ -0,0 +1,10 @@
+function Favourites() {
+  return (
+    <div className="favourites-empty">
+      <h2>No favourite animes yet</h2>
+      <p>Start adding animes to your favourites and they will appear here</p>
+    </div>
+  );
+}
+
+export default Favourites;
