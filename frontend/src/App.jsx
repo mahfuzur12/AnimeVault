316e9c3d61e337c8dcb3615b42254ee2906db531
Mahfuzur Rahman <mxhfxzur.r@gmail.com>
2025-02-20 18:59:10 +0000

    homepage wip

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,10 @@
 import "./App.css";
-import AnimeCard from "./components/AnimeCard";
+import Home from "./pages/Home";
 
 function App() {
-  const movieNumber = 1;
-
   return (
     <>
-      {movieNumber === 1 ? (
-        <AnimeCard anime={{ title: "First Anime", release_date: "2025" }} />
-      ) : (
-        <AnimeCard anime={{ title: "Second Anime", release_date: "2025" }} />
-      )}
+      <Home />
     </>
   );
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import AnimeCard from "../components/AnimeCard";
+import { useState } from "react";
+
+function Home() {
+  const [searchedAnime, setSearchedAnime] = useState("");
+
+  const animes = [
+    { id: 1, title: "Hunter x Hunter", release_date: "2011" },
+    { id: 2, title: "Naruto", release_date: "2003" },
+    { id: 3, title: "Jujutsu Kaisen", release_date: "2020" },
+  ];
+
+  function handleSearch() {}
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for animes"
+          className="search-input"
+          value={searchedAnime}
+          onChange={(e) => setSearchedAnime(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
+      <div className="animes-grid">
+        {animes.map((anime) => (
+          <AnimeCard anime={anime} key={anime.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
