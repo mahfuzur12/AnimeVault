9b61106d54f6ca737b0e56bde09d7451af04836c
Mahfuzur Rahman <mxhfxzur.r@gmail.com>
2025-02-20 19:38:21 +0000

    NavBar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 import Favourites from "./pages/Favourites";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favourites" element={<Favourites />} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
